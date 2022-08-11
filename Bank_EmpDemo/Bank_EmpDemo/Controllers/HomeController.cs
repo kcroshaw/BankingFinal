ea42bcf7c6cd6716b118320420fb517c288535a0
CS-EDU921 <84093555+CS-EDU921@users.noreply.github.com>
2022-08-11 16:33:32 -0600

    add demo
    fix code to work with broken sql server,  this code is a demo and not working

---
+++ Bank_EmpDemo/Bank_EmpDemo/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bank_EmpDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult FullTransactionHistory()
+        {
+            return View();
+        }
+
+        public IActionResult CheckingTransactionHistory()
+        {
+            return View();
+        }
+
+        public IActionResult SavingsTransactionHistory()
+        {
+            return View();
+        }
+
+        public IActionResult LoanTransactionHistory()
+        {
+            return View();
+        }
+
+    }
+}
